339694898dcd987fe59634dd1c747aaa2d1d1774
Booey <124066239+Booey-2468@users.noreply.github.com>
2025-03-20 16:33:19 +0000

    Cooridors completed just need to make sure they stop when overlapping

--- Assets/GenerateCooridor.cs
+++ Assets/GenerateCooridor.cs
@@ -23,6 +23,7 @@ public class GenerateCooridor : MonoBehaviour
     private Direction currentDirection;
     private int moveNum = 1;
     private bool isCorner = false;
+    int cooridorCount = 0;
     public List<Vector3Int> cooridorPos = new List<Vector3Int>();
     public List<Vector3Int> cardinalDirections;
     public bool valuesAssigned = false;
@@ -77,11 +78,15 @@ public class GenerateCooridor : MonoBehaviour
         }
         else
         {
-            int turnChance = UnityEngine.Random.Range(1, 10);
+            int turnChance = UnityEngine.Random.Range(1, 100);
+            int leftOrRight = UnityEngine.Random.Range(0, 2);
+            bool turnLeft = false;
+            Sprite sprite1 = null;
+            Sprite sprite2 = null;
+            List<Sprite> turningSpriteList = new List<Sprite>() {null, null, null};
 
-            if (turnChance >= 7)
-            {
-            }
+
+
             switch (currentDirection)
             {
                 case Direction.left:
@@ -89,14 +94,56 @@ public class GenerateCooridor : MonoBehaviour
                     pos2 = new Vector3Int(currentPos[0] - moveNum, currentPos[1], 0);
                     pos3 = new Vector3Int(currentPos[0] - moveNum, currentPos[1] - moveNum, 0);
 
-                    SetCooridor(pos1, pos2, pos3, bottomWall, topWall);
+                    sprite1 = bottomWall;
+                    sprite2 = topWall;
+
+                    if (leftOrRight == 1 && turnChance > 75 && cooridorCount > 3)
+                    {
+                        turnLeft = true;
+                        turningSpriteList = new List<Sprite>() { bottomWall, topLeftCorner, rightWall };
+
+                        List<Vector3Int> returnStore = SetCorner(pos1, pos2, pos3, currentDirection, turnLeft, turningSpriteList[0], turningSpriteList[1], turningSpriteList[2]);
+                        pos1 = returnStore[0]; pos2 = returnStore[1]; pos3 = returnStore[2]; 
+                        
+                        currentDirection = Direction.down;
+                    }
+                    else if (leftOrRight == 0 && turnChance > 75 && cooridorCount > 3)
+                    {
+                        turningSpriteList = new List<Sprite>() { topWall, bottomLeftCorner, rightWall };
+
+                        List<Vector3Int> returnStore = SetCorner(pos1, pos2, pos3, currentDirection, turnLeft, turningSpriteList[0], turningSpriteList[1], turningSpriteList[2]);
+                        pos1 = returnStore[0]; pos2 = returnStore[1]; pos3 = returnStore[2];
+
+                        currentDirection = Direction.up;
+                    }
                     break;
                 case Direction.up:
                     pos1 = new Vector3Int(currentPos[0] - moveNum, currentPos[1] + moveNum, 0);
                     pos2 = new Vector3Int(currentPos[0], currentPos[1] + moveNum, 0);
                     pos3 = new Vector3Int(currentPos[0] + moveNum, currentPos[1] + moveNum, 0);
 
-                    SetCooridor(pos1, pos2, pos3, rightWall, leftWall);
+                    sprite1 = rightWall;
+                    sprite2 = leftWall;
+
+                    if (leftOrRight == 1 && turnChance > 75 && cooridorCount > 3)
+                    {
+                        turnLeft = true;
+                        turningSpriteList = new List<Sprite>() { leftWall, topRightCorner, bottomWall };
+
+                        List<Vector3Int> returnStore = SetCorner(pos1, pos2, pos3, currentDirection, turnLeft, turningSpriteList[0], turningSpriteList[1], turningSpriteList[2]);
+                        pos1 = returnStore[0]; pos2 = returnStore[1]; pos3 = returnStore[2];
+
+                        currentDirection = Direction.left;
+                    }
+                    else if (leftOrRight == 0 && turnChance > 75 && cooridorCount > 3)
+                    {
+                        turningSpriteList = new List<Sprite>() { rightWall, topLeftCorner, bottomWall };
+
+                        List<Vector3Int> returnStore = SetCorner(pos1, pos2, pos3, currentDirection, turnLeft, turningSpriteList[0], turningSpriteList[1], turningSpriteList[2]);
+                        pos1 = returnStore[0]; pos2 = returnStore[1]; pos3 = returnStore[2];
+
+                        currentDirection = Direction.right;
+                    }
                     break;
                 case Direction.right:
 
@@ -104,16 +151,68 @@ public class GenerateCooridor : MonoBehaviour
                     pos2 = new Vector3Int(currentPos[0] + moveNum, currentPos[1], 0);
                     pos3 = new Vector3Int(currentPos[0] + moveNum, currentPos[1] - moveNum, 0);
 
-                    SetCooridor(pos1, pos2, pos3, bottomWall, topWall);
+                    sprite1 = bottomWall;
+                    sprite2 = topWall;
+
+                    if (leftOrRight == 1 && turnChance > 75 && cooridorCount > 3)
+                    {
+                        turnLeft = true;
+                        turningSpriteList = new List<Sprite>() { topWall, bottomLeftCorner, leftWall };
+
+                        List<Vector3Int> returnStore = SetCorner(pos1, pos2, pos3, currentDirection, turnLeft, turningSpriteList[0], turningSpriteList[1], turningSpriteList[2]);
+                        pos1 = returnStore[0]; pos2 = returnStore[1]; pos3 = returnStore[2];
+
+                        currentDirection = Direction.up;
+                    }
+                    else if (leftOrRight == 0 && turnChance > 75 && cooridorCount > 3)
+                    {
+                        turningSpriteList = new List<Sprite>() { bottomWall, topLeftCorner, leftWall };
+
+                        List<Vector3Int> returnStore = SetCorner(pos1, pos2, pos3, currentDirection, turnLeft, turningSpriteList[0], turningSpriteList[1], turningSpriteList[2]);
+                        pos1 = returnStore[0]; pos2 = returnStore[1]; pos3 = returnStore[2];
+
+                        currentDirection = Direction.down;
+                    }
                     break;
                 case Direction.down:
                     pos1 = new Vector3Int(currentPos[0] - moveNum, currentPos[1] - moveNum, 0);
                     pos2 = new Vector3Int(currentPos[0], currentPos[1] - moveNum, 0);
                     pos3 = new Vector3Int(currentPos[0] + moveNum, currentPos[1] - moveNum, 0);
 
-                    SetCooridor(pos1, pos2, pos3, rightWall, leftWall);
+                    sprite1 = rightWall;
+                    sprite2 = leftWall;
+
+                    if (leftOrRight == 1 && turnChance > 75 && cooridorCount > 3)
+                    {
+                        turnLeft = true;
+                        turningSpriteList = new List<Sprite>() { rightWall, bottomLeftCorner, topWall };
+
+                        List<Vector3Int> returnStore = SetCorner(pos1, pos2, pos3, currentDirection, turnLeft, turningSpriteList[0], turningSpriteList[1], turningSpriteList[2]);
+                        pos1 = returnStore[0]; pos2 = returnStore[1]; pos3 = returnStore[2];
+
+                        currentDirection = Direction.right;
+                    }
+                    else if(leftOrRight == 0 && turnChance > 75 && cooridorCount > 3)
+                    {
+                        turningSpriteList = new List<Sprite>() { leftWall, bottomRightCorner, topWall };
+
+                        List<Vector3Int> returnStore = SetCorner(pos1, pos2, pos3, currentDirection, turnLeft, turningSpriteList[0], turningSpriteList[1], turningSpriteList[2]);
+                        pos1 = returnStore[0]; pos2 = returnStore[1]; pos3 = returnStore[2];
+
+                        currentDirection = Direction.left;
+                    }
                     break;
             }
+            if (!(cooridorCount > 3 && turnChance > 7))
+            {
+                SetCooridor(pos1, pos2, pos3, sprite1, sprite2);
+                cooridorCount++;
+            }
+            else
+            {
+                cooridorCount = 0;
+            }
+
             currentPos = pos2;
         }
     }
@@ -127,54 +226,77 @@ public class GenerateCooridor : MonoBehaviour
         cooridorPos.Add(pos2);
         cooridorPos.Add(pos3);
     }
-    private void SetCorner(Vector3Int pos1, Vector3Int pos2, Vector3Int pos3, Direction currentDirection, bool turnLeft, Sprite sideSprite, Sprite cornerSprite, Sprite upperSprite)
+    private List<Vector3Int> SetCorner(Vector3Int pos1, Vector3Int pos2, Vector3Int pos3, Direction initialDirection, bool turnLeft, Sprite sideSprite, Sprite cornerSprite, Sprite upperSprite)
     {
         Vector3Int localUp = new Vector3Int(0,0,0);
         Vector3Int localDown = new Vector3Int(0,0,0);
-        switch(currentDirection)
+        Vector3Int localLeft = new Vector3Int(0, 0, 0);
+        Vector3Int localRight = new Vector3Int(0, 0, 0);
+        switch (initialDirection)
         {
             case Direction.up:
-                localUp = new Vector3Int(0,-1,0);
-                localDown = new Vector3Int(0,1,0);
+                localUp = new Vector3Int(0,1,0);
+                localDown = new Vector3Int(0,-1,0);
+                localLeft = new Vector3Int(-1, 0, 0);
+                localRight = new Vector3Int(1, 0, 0);
                 break;
             case Direction.down:
-                localUp = new Vector3Int(0, 1, 0);
-                localDown = new Vector3Int(0, -1, 0);
+                localUp = new Vector3Int(0, -1, 0);
+                localDown = new Vector3Int(0, 1, 0);
+                localLeft = new Vector3Int(1, 0, 0);
+                localRight = new Vector3Int(-1, 0, 0);
                 break;
             case Direction.left:
                 localUp = new Vector3Int(-1, 0, 0);
                 localDown = new Vector3Int(1, 0, 0);
+                localLeft = new Vector3Int(0, -1, 0);
+                localRight = new Vector3Int(0, 1, 0);
                 break;
             case Direction.right:
                 localUp = new Vector3Int(1, 0, 0);
                 localDown = new Vector3Int(-1, 0, 0);
+                localLeft = new Vector3Int(0, 1, 0);
+                localRight = new Vector3Int(0, -1, 0);
                 break;
         }
         wallsTilemap.SetTile(pos2, null);
         if (turnLeft)
         {
-            wallsTilemap.SetTile(pos3, new Tile() { sprite = sideSprite });
-            wallsTilemap.SetTile(pos1, null);
+            wallsTilemap.SetTile(pos2 + localRight, new Tile() { sprite = sideSprite });
+            wallsTilemap.SetTile(pos2 + localLeft, null);
 
-            wallsTilemap.SetTile(pos1 + localUp, new Tile() { sprite = cornerSprite });
-            wallsTilemap.SetTile(pos3 + localDown, new Tile() { sprite = cornerSprite });
+
+            wallsTilemap.SetTile(pos2 + localRight + localUp, new Tile() { sprite = cornerSprite });
+
+            wallsTilemap.SetTile(pos2 + localUp, new Tile() { sprite = upperSprite });
+
+            pos2 += localLeft;
+            pos1 = pos2 + localUp;
+            pos3 = pos2 + localDown;
+
         }
         else
         {
-            
-            wallsTilemap.SetTile(pos1, new Tile() { sprite = sideSprite });
-            wallsTilemap.SetTile(pos3, null);
+            wallsTilemap.SetTile(pos2 + localLeft, new Tile() { sprite = sideSprite });
 
+            wallsTilemap.SetTile(pos2 + localRight, null);
 
-            wallsTilemap.SetTile(pos3 + localUp, new Tile() { sprite = cornerSprite });
-            wallsTilemap.SetTile(pos1 + localDown, new Tile() { sprite = cornerSprite });
+            wallsTilemap.SetTile(pos2 + localRight + localDown, new Tile() { sprite = cornerSprite });
+
+            wallsTilemap.SetTile(pos2 + localUp, new Tile() { sprite = upperSprite });
+
+            pos2 += localRight;
+            pos1 = pos2 + localUp;
+            pos3 = pos2 + localDown;
 
         }
+        wallsTilemap.SetTile(pos1, new Tile() { sprite = upperSprite });
 
-        wallsTilemap.SetTile(pos2 + cardinalDirections[2], new Tile() { sprite = upperSprite });
 
         cooridorPos.Add(pos1);
         cooridorPos.Add(pos2);
         cooridorPos.Add(pos3);
+        return new List<Vector3Int>() { pos1, pos2, pos3 };
+
     }
 }
